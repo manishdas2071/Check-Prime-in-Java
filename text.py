5bb70c4a9034c59a89c99b6e33585b915c528635
Manjit Kumar Das <manjit6kumar@gmail.com>
2025-11-01 20:13:34 +0530

    Update text.py
    added the main part

--- text.py
+++ text.py
@@ -2,7 +2,14 @@
 
 
 
-
+else:
+  for i in rang (2, num):
+    if (num%i == 0):
+      tag = 1
+  if (tag == 0):
+    print(f"The number {num} is prime")
+  else:
+    print(f"The number {num} is  not prime")
 
 
 
